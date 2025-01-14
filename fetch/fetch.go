175df829fb3d5f9803218e5f5c5e36c44c37fcc0
KArtem <k6art5@gmail.com>
2025-01-14 22:59:29 +0700

    added in fetch ReadAll method

--- fetch/fetch.go
+++ fetch/fetch.go
@@ -2,15 +2,24 @@ package fetch
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
-func Get(url string) (resp *http.Response) {
+func Get(url string) []byte {
 	resp, err := http.Get(url)
 
 	if err != nil {
 		fmt.Println("Error in Get fetch:", err)
 	}
 
-	return resp
+	b, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+		os.Exit(1)
+	}
+
+	return b
 }
