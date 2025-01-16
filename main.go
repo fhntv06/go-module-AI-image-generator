67a30f9232c575f02b8cbc1c78abc5d33ac2ecf3
KArtem <k6art5@gmail.com>
2025-01-16 23:20:46 +0700

    wip main

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	api "AI_image_generator/basic/api"
 	db "AI_image_generator/basic/database"
 	env "AI_image_generator/basic/env_handler"
 	fetch "AI_image_generator/basic/fetch"
@@ -20,12 +21,16 @@ func InitialApp() {
 }
 
 func main() {
+	// important DONT REMOVE / REPLACE
 	InitialApp()
+
+	api.ApiKandinsky()
+	return
 
 	dbLocal := db.ConstructorDB()
 	db.CreateNewTables(dbLocal, "images")
 
-	googlePage := fmt.Sprintf("%s", fetch.Get("https://www.google.com"))
+	googlePage := fmt.Sprintf("%s", fetch.Get("https://www.google.com", nil))
 
 	file.CreateDirToPath("google/")
 	f := file.OpenFile("google/index.html")
